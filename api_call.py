8985fb9494ecc381c08110a716292f42354a50cf
Daniel Vejsada <dan.vejsada@gmail.com>
2024-02-10 00:36:39 +0100

    Switch from ids to aswIds as stop identifier.

--- api_call.py
+++ api_call.py
@@ -9,7 +9,7 @@ class ApiCall:
     @staticmethod
     def update_info(api_key, stop_id, conn_num):
         headers = {"Content-Type": "application/json; charset=utf-8", "x-access-token": api_key}
-        parameters = {"ids": stop_id, "total": conn_num, "minutesAfter": 720}
+        parameters = {"aswIds": stop_id, "total": conn_num, "minutesAfter": 720}
 
         response = requests.get(API_URL, params=parameters, headers=headers)
         return response.json()
@@ -17,10 +17,10 @@ class ApiCall:
     @staticmethod
     def authenticate(api_key, stop_id, conn_num):
         headers = {"Content-Type": "application/json; charset=utf-8", "x-access-token": api_key}
-        parameters = {"ids": stop_id, "total": conn_num, "minutesAfter": 720}
+        parameters = {"aswIds": stop_id, "total": conn_num, "minutesAfter": 720}
         response = requests.get(API_URL, params=parameters, headers=headers)
         reply = response.json()
-        title = reply["stops"][0]["stop_name"]
+        title = reply["stops"][0]["stop_name"] + reply["stops"][0]["platform_code"]
         if response.status_code == 200:
             return True, title
         else:
