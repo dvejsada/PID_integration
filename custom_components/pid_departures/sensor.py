39909f908320b739255195c79240527e3fb0a8f4
Jakub Jirutka <jakub@jirutka.cz>
2024-07-10 21:18:55 +0200

    Add sensor for departure time

--- custom_components/pid_departures/sensor.py
+++ custom_components/pid_departures/sensor.py
@@ -32,6 +32,7 @@ async def async_setup_entry(
     # Set entities for departures
     for i in range(departure_board.conn_num):
         new_entities.append(RouteNameSensor(departure_board, i))
+        new_entities.append(DepartureTimeSensor(departure_board, i))
 
     # Set diagnostic entities
     new_entities.append(StopSensor(departure_board))
@@ -86,6 +87,40 @@ class RouteNameSensor(BaseEntity, SensorEntity):
         self._departure_board.register_callback(self.async_write_ha_state)
 
     async def async_will_remove_from_hass(self) -> None:
+        """Entity being removed from hass."""
+        # The opposite of async_added_to_hass. Remove any registered call backs here.
+        self._departure_board.remove_callback(self.async_write_ha_state)
+
+
+class DepartureTimeSensor(BaseEntity, SensorEntity):
+    """Sensor for the next departure time (estimated)."""
+
+    _attr_translation_key = "departure_time"
+    _attr_should_poll = False
+    _attr_device_class = SensorDeviceClass.TIMESTAMP
+
+    def __init__(self, departure_board: DepartureBoard, departure_num: int) -> None:
+        super().__init__(departure_board)
+        self._departure_num = departure_num
+        self._attr_unique_id = f"{departure_board.board_id}_{self.translation_key}_{departure_num + 1}"
+        self._attr_translation_placeholders = {"num": str(departure_num + 1)}
+
+    @property
+    def native_value(self) -> datetime | None:
+        return self._departure_board.departures[self._departure_num].departure_time_est
+
+    @property
+    def icon(self) -> str:
+        """Returns entity icon based on the type of route"""
+        route_type = self._departure_board.departures[self._departure_num].route_type
+        return ROUTE_TYPE_ICON.get(route_type, ROUTE_TYPE_ICON[RouteType.BUS])
+
+    async def async_added_to_hass(self):
+        """Run when this Entity has been added to HA."""
+        # Sensors should also register callbacks to HA when their state changes
+        self._departure_board.register_callback(self.async_write_ha_state)
+
+    async def async_will_remove_from_hass(self):
         """Entity being removed from hass."""
         # The opposite of async_added_to_hass. Remove any registered call backs here.
         self._departure_board.remove_callback(self.async_write_ha_state)
